21e448baa00bd51ae1606c9c34e9ab0b1e6cfc2a
lowe1111 <lorenz.weiss@loopline-systems.com>
2020-03-02 21:22:51 +0100

    direction and build example

--- src/Questions.tsx
+++ src/Questions.tsx
@@ -8,40 +8,55 @@ interface IProps {
   onFinish: () => void;
   children: (nextCard: any, currentIndex: number) => ReactElement;
   animation?: "blur-fade" | "grow-fade";
+  renderNavigation?: (
+    next: () => void,
+    prev: () => void,
+    index: number,
+  ) => ReactElement;
 }
 
-const Questions: FC<IProps> = ({ questions, onFinish, children, animation }) => {
+const Questions: FC<IProps> = (props) => {
+  const { questions, onFinish, children, animation, renderNavigation } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isIn, setIsIn] = useState(true);
+  const [direction, setDirection] = useState("next");
 
   const currentQuestion =
     currentIndex < questions.length ? questions[currentIndex] : null;
 
   const inProgress = useRef(false);
   
-  const nextCard = async () => {
+  const moveCard = async (moveDirection: "next" | "prev") => {
     if (inProgress.current) return;
     // block the animation if animation is in progress
     inProgress.current = true;
+    setDirection(moveDirection)
     // delay of the submit
     await sleep(250);
     setIsIn(false);
     // delay of the animation
     await sleep(500);
     setIsIn(true);
-    setNextQuestion(currentIndex);
+    if (moveDirection === "next") {
+      setNextQuestion();
+    } else {
+      setPrevQuestion();
+    }
     inProgress.current = false;
-  };
+  }
 
-  const setNextQuestion = (index: number) => {
-    const nextQuestionIndex = index + 1;
-    // are we done? finish it
-    if (nextQuestionIndex >= questions.length) {
+  const setPrevQuestion = () => {
+    if (currentIndex <= 0) return;
+    setCurrentIndex(currentIndex - 1);
+  }
+
+  const setNextQuestion = () => {
+    if (currentIndex + 1 >= questions.length) {
       onFinishQuestionnaire();
       return
     }
     // finally update the index
-    setCurrentIndex(nextQuestionIndex);
+    setCurrentIndex(currentIndex + 1);
   };
 
   const onFinishQuestionnaire = () => {
@@ -50,18 +65,27 @@ const Questions: FC<IProps> = ({ questions, onFinish, children, animation }) => 
     }
   };
 
+  const next = () => {
+    moveCard("next")
+  }
+
+  const prev = () => {
+    moveCard("prev")
+  }
+
   return (
-    <div>
+    <div className="container">
       {currentQuestion && (
         <Transition in={isIn} timeout={500}>
           {(status: any) => (
-            <div className={`animateur ${status} ${animation || "blur-fade"}`}>
-              <div className="content">
-                {children(nextCard, currentIndex)}
-              </div>
+            <div className={`animateur ${direction} ${status} ${animation || "blur-fade"}`}>
+              {children(next, currentIndex)}
             </div>
           )}
         </Transition>
+      )}
+      {renderNavigation && (
+        <div className="navigation">{renderNavigation(next, prev, currentIndex)}</div>
       )}
     </div>
   );
